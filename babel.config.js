c6839b912040a912d5e60d5b4509f096d192f4e9
AsadDevsinc <asad.yasin@devsinc.com>
2022-11-21 15:48:39 +0500

    Initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function(api) {
   api.cache(true);
   return {
+
     presets: ['module:metro-react-native-babel-preset'],
     plugins: ['@babel/plugin-proposal-class-properties'],
+    
   };
 };
+
